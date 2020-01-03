6b056984d281111aa14e837acf9e3704dd31d955
Fripley <filipecheverrya@gmail.com>
2020-01-03 20:10:50 -0300

    Updates

---
+++ precache-manifest.0a12641fabdc2e1b572161010a3b4333.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdea932d4a620fc8119802db5eaee29f",
+    "url": "/self-service-restaurant/index.html"
+  },
+  {
+    "revision": "3bd9ce4439c2e764a6ff",
+    "url": "/self-service-restaurant/static/js/2.80a62638.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/self-service-restaurant/static/js/2.80a62638.chunk.js.LICENSE"
+  },
+  {
+    "revision": "622a22a66b309fed4b8e",
+    "url": "/self-service-restaurant/static/js/main.6d7eabfb.chunk.js"
+  },
+  {
+    "revision": "2cd0a4f4943c278b7503",
+    "url": "/self-service-restaurant/static/js/runtime-main.d76725e9.js"
+  }
+]);
